3dfa9fa70d746ffc999936940ba161d7d6a6133b
Nam Vu <vunhatnam19497@gmail.com>
2024-03-27 06:35:03 +0000

    modify step_trainer_trusted file

--- glue_script/step_trainer_trusted.py
+++ glue_script/step_trainer_trusted.py
@@ -5,26 +5,64 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init(args['JOB_NAME'], args)
-
-# Script generated for node Customer Curated
-CustomerCurated_node1711511557922 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://namnhatvu-stedi-lakehouse/customer/curated/"], "recurse": True}, transformation_ctx="CustomerCurated_node1711511557922")
+job.init(args["JOB_NAME"], args)
 
 # Script generated for node Step Trainer Landing
-StepTrainerLanding_node1711511600226 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://namnhatvu-stedi-lakehouse/step_trainer/landing/"], "recurse": True}, transformation_ctx="StepTrainerLanding_node1711511600226")
+StepTrainerLanding_node1711519749726 = glueContext.create_dynamic_frame.from_catalog(
+    database="namnhatvu_stedi_lakehouse",
+    table_name="step_trainer_landing",
+    transformation_ctx="StepTrainerLanding_node1711519749726",
+)
 
-# Script generated for node Mapping Join
-MappingJoin_node1711511645952 = Join.apply(frame1=StepTrainerLanding_node1711511600226, frame2=CustomerCurated_node1711511557922, keys1=["serialnumber"], keys2=["serialnumber"], transformation_ctx="MappingJoin_node1711511645952")
+# Script generated for node Customer Curated
+CustomerCurated_node1711519789617 = glueContext.create_dynamic_frame.from_catalog(
+    database="namnhatvu_stedi_lakehouse",
+    table_name="customer_curated",
+    transformation_ctx="CustomerCurated_node1711519789617",
+)
+
+# Script generated for node Apply Join
+ApplyJoin_node1711519857292 = Join.apply(
+    frame1=StepTrainerLanding_node1711519749726,
+    frame2=CustomerCurated_node1711519789617,
+    keys1=["serialnumber"],
+    keys2=["serialnumber"],
+    transformation_ctx="ApplyJoin_node1711519857292",
+)
 
 # Script generated for node Drop Fields
-DropFields_node1711511716601 = DropFields.apply(frame=MappingJoin_node1711511645952, paths=["serialnumber", "sensorreadingtime", "`.serialnumber`", "distancefromobject"], transformation_ctx="DropFields_node1711511716601")
+DropFields_node1711520589325 = DropFields.apply(
+    frame=ApplyJoin_node1711519857292,
+    paths=[
+        "customername",
+        "email",
+        "phone",
+        "birthday",
+        "`.serialnumber`",
+        "registrationdate",
+        "lastupdatedate",
+        "sharewithresearchasofdate",
+        "sharewithpublicasofdate",
+        "sharewithfriendsasofdate",
+    ],
+    transformation_ctx="DropFields_node1711520589325",
+)
 
 # Script generated for node Step Trainer Trusted
-StepTrainerTrusted_node1711511800643 = glueContext.write_dynamic_frame.from_options(frame=DropFields_node1711511716601, connection_type="s3", format="json", connection_options={"path": "s3://namnhatvu-stedi-lakehouse/step_trainer/trusted/", "partitionKeys": []}, transformation_ctx="StepTrainerTrusted_node1711511800643")
+StepTrainerTrusted_node1711520740136 = glueContext.write_dynamic_frame.from_options(
+    frame=DropFields_node1711520589325,
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "path": "s3://namnhatvu-stedi-lakehouse/step_trainer/trusted/",
+        "partitionKeys": [],
+    },
+    transformation_ctx="StepTrainerTrusted_node1711520740136",
+)
 
 job.commit()
